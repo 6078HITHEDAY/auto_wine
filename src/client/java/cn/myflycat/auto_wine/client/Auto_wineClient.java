e9faf43529b58576956ea62eb55b12c30e8c5475
myflycat <88145214@qq.com>
2026-04-11 20:05:04 +0800

    Initial commit

---
+++ src/client/java/cn/myflycat/auto_wine/client/Auto_wineClient.java
@@ -0,0 +1,10 @@
+package cn.myflycat.auto_wine.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Auto_wineClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cn/myflycat/auto_wine/Auto_wine.java
@@ -0,0 +1,10 @@
+package cn.myflycat.auto_wine;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Auto_wine implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
